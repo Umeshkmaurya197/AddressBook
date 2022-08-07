361e1e9cee590627f1c0c70ecebc9f15e6bc28ee
Umeshkmaurya197 <umeshkumarmaurya.98@gmail.com>
2022-08-07 22:57:52 +0530

    Refactored : editContact method added in addressbook.java file

--- src/com/addressbook/streamapi/AddressBook.java
+++ src/com/addressbook/streamapi/AddressBook.java
@@ -4,30 +4,32 @@ import java.util.Scanner;
 
 public class AddressBook {
 	private Contact contact;
-	private Scanner scanner = new Scanner(System.in);
+	private static Scanner scanner = new Scanner(System.in);
+	static AddressBook addressBook;
 
+	// add new Contact -uc1
 	public Contact addContact() {
 		System.out.print("Enter id  : ");
 		int id = scanner.nextInt();
-		
+
 		System.out.print("Enter First Name  : ");
 		String firstName = scanner.next();
-		
+
 		System.out.print("Enter Last Name  : ");
 		String lastName = scanner.next();
-		
+
 		System.out.print("Enter Address  : ");
 		String address = scanner.next();
-		
+
 		System.out.print("Enter City  : ");
 		String city = scanner.next();
-		
+
 		System.out.print("Enter State  : ");
 		String state = scanner.next();
-		
+
 		System.out.print("Enter Zip  : ");
 		int zip = scanner.nextInt();
-		
+
 		System.out.print("Enter Phone Number  : ");
 		String phoneNumber = scanner.next();
 
@@ -35,12 +37,72 @@ public class AddressBook {
 		return contact;
 	}
 
+	// edit exiting contact -uc2
+	private Contact editContact(String fName) {
+		if (contact.getFirstName().equals(fName)) {
+			System.out.print("Enter First Name  : ");
+			String firstName = scanner.next();
+
+			System.out.print("Enter Last Name  : ");
+			String lastName = scanner.next();
+
+			System.out.print("Enter Address  : ");
+			String address = scanner.next();
+
+			System.out.print("Enter City  : ");
+			String city = scanner.next();
+
+			System.out.print("Enter State  : ");
+			String state = scanner.next();
+
+			System.out.print("Enter Zip  : ");
+			int zip = scanner.nextInt();
+
+			System.out.print("Enter Phone Number  : ");
+			String phoneNumber = scanner.next();
+
+			contact.setFirstName(firstName);
+			contact.setLastName(lastName);
+			contact.setAddress(address);
+			contact.setCity(city);
+			contact.setState(state);
+			contact.setZip(zip);
+			contact.setPhoneNumber(phoneNumber);
+			return contact;
+		} else
+			System.out.println(" Contact Not found ");
+		return contact;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("-----------Welcome to Address Book---------");
 
-		AddressBook addressBook = new AddressBook();
-		System.out.println("::::: add Contact::::::");
-		System.out.println(addressBook.addContact());
+		addressBook = new AddressBook();
+		String exit = "n";
+		while (exit != "y") {
+			System.out.println("Enter 1 to add new Contact ");
+			System.out.println("Enter 2 to update existing contact ");
+			System.out.println("Enter 3 to Exit ");
+			System.out.print("Enter option : ");
+			int input = scanner.nextInt();
+			switch (input) {
+			case 1:
+				System.out.println(":::::: add Contact ::::::");
+				System.out.println(addressBook.addContact());
+				break;
+			case 2:
+				System.out.println(":::::: edit Contact ::::::");
+				System.out.print(" Enter first name to edit contact ");
+				String firstName = scanner.next();
+				System.out.println(addressBook.editContact(firstName));
+				break;
+			case 3:
+				System.out.println(":::::: exit from address book ::::::");
+				exit = "y";
+				break;
+			}
+		}
+		System.out.println("exited ");
 
 	}
 
